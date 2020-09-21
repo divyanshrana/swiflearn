d1f6098383c5db7f7c67698605b5c1d0cce17936
Divyansh <divyansh.rana12@gmail.com>
2020-09-21 12:49:42 +0530

    Updates

---
+++ precache-manifest.1b4153002c2c4ce45f00fdee6f1e6746.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f1bcad7b72a1519175d540233cf7833",
+    "url": "/swiflearn/index.html"
+  },
+  {
+    "revision": "2b4ec3c44d3cbb912396",
+    "url": "/swiflearn/static/css/main.4ec64df1.chunk.css"
+  },
+  {
+    "revision": "dc2d3f7723ed0417e7c9",
+    "url": "/swiflearn/static/js/2.03cc669b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/swiflearn/static/js/2.03cc669b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b4ec3c44d3cbb912396",
+    "url": "/swiflearn/static/js/main.d9c5ae1d.chunk.js"
+  },
+  {
+    "revision": "19e3f23cccb720d9ee09",
+    "url": "/swiflearn/static/js/runtime-main.93824cb1.js"
+  },
+  {
+    "revision": "6544b1015d3315a7888f80b5ac1208d7",
+    "url": "/swiflearn/static/media/live.6544b101.ico"
+  },
+  {
+    "revision": "4aac3d235682cc9a0cb05c4682c55eb4",
+    "url": "/swiflearn/static/media/youtube.4aac3d23.webp"
+  }
+]);
